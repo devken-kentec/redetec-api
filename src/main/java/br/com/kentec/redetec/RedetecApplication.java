ea55569edab99b7cbf4cfcf2690dc128b224ec30
devken-kentec <kennedy_tomazete@outlook.com>
2024-03-15 15:39:49 -0300

    'Primeiro'

---
+++ src/main/java/br/com/kentec/redetec/RedetecApplication.java
@@ -0,0 +1,13 @@
+package br.com.kentec.redetec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedetecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedetecApplication.class, args);
+	}
+
+}
